4285d9b7c05aab0e0539204ae6f62b924139653e
gurusudhan25 <gurusudhan25@gmail.com>
2022-09-29 19:28:30 +0530

    Second commit

---
+++ Assignments/Gurusudhan/Assignment_2/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/logout")
+def logout():
+    return render_template('logout.html')
+if __name__=="main":
+    app.run(debug=True)
