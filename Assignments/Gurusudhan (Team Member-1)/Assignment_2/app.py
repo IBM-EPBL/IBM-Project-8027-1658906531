c3299c3dce07eccc460987e75ff3a9383364297f
gurusudhan25 <gurusudhan25@gmail.com>
2022-11-12 23:10:47 +0530

    feat: new updates

--- Assignments/Gurusudhan (Team Member-1)/Assignment_2/app.py
+++ Assignments/Gurusudhan (Team Member-1)/Assignment_2/app.py
@@ -1,21 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/login")
 def login():
     return render_template('login.html')
+
 
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
 
+
 @app.route("/logout")
 def logout():
     return render_template('logout.html')
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=True)
