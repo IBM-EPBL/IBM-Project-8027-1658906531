bca405958bca2e57e403a7a3789ded077622c365
gurusudhan25 <gurusudhan25@gmail.com>
2022-11-13 11:15:07 +0530

    feat: UI updated

--- Assignments/Gurusudhan (Team Member-1)/Assignment_2/app.py
+++ Assignments/Gurusudhan (Team Member-1)/Assignment_2/app.py
@@ -1,26 +1,15 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
-
-
-@app.route("/login")
-def login():
-    return render_template('login.html')
+    return render_template("Login.html")
 
 
 @app.route("/signup")
 def signup():
-    return render_template('signup.html')
-
-
-@app.route("/logout")
-def logout():
-    return render_template('logout.html')
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
